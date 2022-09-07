c679954ef9dd149d7db4454e792e8180ce77f832
buburuzaaaaa <110576807+buburuzaaaaa@users.noreply.github.com>
2022-09-06 18:03:21 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+//this is the code for the users input to binary
+let khadeem = parseInt(prompt("enter any number"))
+let bernard = khadeem.toString(2)
+// this is the hex answer for the users input
+let mec= khadeem.toString(16)
+
+alert('binary '+bernard)
+alert('hexadecimal '+ mec)
+
+ 
